9f1c225246d7d38a44eda17bb1c0d482c21a3794
Jay <10658010+xXTheBigBearXx@users.noreply.github.com>
2025-07-03 10:48:44 +0100

    Update setup.php
    Translates comments & GLPI menu items to English.
    Moved the file into root instead of a folder.

--- setup.php
+++ setup.php
@@ -1,25 +1,25 @@
 <?php
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_VALIDATIONAUTO_VERSION', '1.0.0');
+define('PLUGIN_VALIDATIONAUTO_VERSION', '1.0.1');
 
 function plugin_init_validationauto() {
     global $PLUGIN_HOOKS;
     
-    // Necessário para segurança
+    // Necessary for Security.
     $PLUGIN_HOOKS[Hooks::CSRF_COMPLIANT]['validationauto'] = true;
     
     if (Session::haveRight('config', UPDATE)) {
-        // Adiciona o menu de configuração
+        // Adds the settings menu.
         $PLUGIN_HOOKS['config_page']['validationauto'] = 'front/config.php';
         
-        // Adiciona o menu ao GLPI
+        // Adds the menu to GLPI.
         $PLUGIN_HOOKS['menu_toadd']['validationauto'] = [
             'config' => 'PluginValidationautoConfig'
         ];
     }
     
-    // Registrar o hook para processar followups
+    // Registers the hook for processing followups. 
     $PLUGIN_HOOKS['item_add']['validationauto'] = [
         'ITILFollowup' => 'plugin_validationauto_process_followup'
     ];
@@ -27,11 +27,11 @@ function plugin_init_validationauto() {
 
 function plugin_version_validationauto() {
     return [
-        'name'           => 'Aprovação Automatica por email',
+        'name'           => 'Automatic Ticket Validation',
         'version'        => PLUGIN_VALIDATIONAUTO_VERSION,
-        'author'         => 'Adriano Marinho',
+        'author'         => 'Adriano Marinho, English Translation by Jay',
         'license'        => 'GLPv3+',
-        'homepage'       => 'https://github.com/malakaygames',
+        'homepage'       => 'https://github.com/Foray-IT/Ticket-Approval-Plugin/',
         'requirements'   => [
             'glpi' => [
                 'min' => '9.5',
@@ -47,7 +47,7 @@ function plugin_version_validationauto() {
 
 function plugin_validationauto_check_prerequisites() {
     if (version_compare(GLPI_VERSION, '9.5', 'lt')) {
-        echo "Este plugin requer GLPI >= 9.5";
+        echo "This plugin requires a GLPI version >= 9.5";
         return false;
     }
     return true;
@@ -55,7 +55,7 @@ function plugin_validationauto_check_prerequisites() {
 
 function plugin_validationauto_check_config($verbose = false) {
     if ($verbose) {
-        echo 'Configuração OK';
+        echo 'Settings OK';
     }
     return true;
 }
