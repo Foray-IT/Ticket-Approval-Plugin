88f874f8443f0f8561ad78faa7c916ec4c661ca7
Jay <10658010+xXTheBigBearXx@users.noreply.github.com>
2025-07-04 13:34:46 +0100

    Bump Version Number

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_VALIDATIONAUTO_VERSION', '1.0.0');
+define('PLUGIN_VALIDATIONAUTO_VERSION', '1.0.4');
 
 function plugin_init_validationauto() {
     global $PLUGIN_HOOKS;
